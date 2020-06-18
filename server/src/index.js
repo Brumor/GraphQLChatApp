c9427cf49c41205633c91632864b961b5cd5b57f
Paul Bricout <paul.bricout@bynk.se>
2020-06-18 12:56:53 +0200

    removed unecessary comment

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,6 @@
 const { GraphQLServer, PubSub } = require("graphql-yoga");
 const typeDefs = require("./schema");
 const resolvers = require("./resolver");
-
-// ref : https://pusher.com/tutorials/chat-graphql-subscriptions
-
 const pubsub = new PubSub();
 const server = new GraphQLServer({ typeDefs, resolvers, context: { pubsub } });
 
